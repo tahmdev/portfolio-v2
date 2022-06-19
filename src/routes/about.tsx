20cdcefca6f55026256d5a594dacc1b8c4989e16
tahmdev <qwertiimusic@gmail.com>
2022-06-19 06:16:05 +0200

    Filled out about me

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -9,16 +9,14 @@ export const About: React.FC<Props> = () => {
       <h1>About me</h1>
       <div className="split">
         <div className="about-me">
-          <h2>Name here</h2>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero
-          pariatur distinctio facilis, voluptate consectetur, nostrum iusto
-          atque veniam, possimus numquam qui sequi nulla! Dolorem totam ipsa
-          magni velit eligendi odio expedita. Harum optio libero ex sapiente
-          maiores ullam commodi tenetur labore numquam, ut qui, tempora debitis
-          vel quae cum est quod architecto rem. Repudiandae alias architecto
-          necessitatibus velit? Sunt porro, deleniti autem quaerat, ea unde
-          repellendus, voluptas{" "}
-          <Link to={"/projects"}> Take a look at some of them → </Link>
+          <h2>Tom Schülke</h2>
+          <p>
+            {`I'm a web developer and language enthusiast based in Germany. 
+            Teaching myself Japanese has made me a great self-reliant learner, and teaching other people has given me a deep understanding on how to communicate effectively.
+            I have built multiple websites from scratch using modern technology. `}
+            <Link to={"/projects"}> Take a look at some of them → </Link>
+            {`\nDon't hesitate to reach out if you have any questions.`}
+          </p>
         </div>
         <div>
           <h2>My skills</h2>
